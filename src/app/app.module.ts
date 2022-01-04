2aacd228b9c8229f762002d7efae0e57ad88a310
Vincent BECHU <vincentbechu@gmail.com>
2022-01-04 15:53:23 +0100

    add fonction on hello service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { HelloService } from './service/hello.service';
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [HelloService,],
   bootstrap: [AppComponent]
--- src/app/service/hello.service.ts
+++ src/app/service/hello.service.ts
@@ -1,15 +1,29 @@
 import { Injectable } from '@angular/core';
+import { NgModule } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HelloService {
 
+  currentLocale = 'fr';
+  messages = {
+    fr: 'Bonjour',
+    en: 'Hello',
+    es: 'Hola',
+    de: 'Halo'
+  }
+
   constructor() { }
 
   greet() {
-    return 'Hello';
+    return this.messages[this.currentLocale];
   }
+
+  getLanguages() {
+    return Object.keys(this.messages);
+  }
+
 }
 
 // commande pour creer des services :
